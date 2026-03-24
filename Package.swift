5e0f84460b7cfc8f9bf9e04629a082aa73748a13
Mehmet Karagöz <eng.mkaragoz@gmail.com>
2026-03-24 21:27:56 +0000

    Fix SPM: use UnityAds binary target instead of source package

--- Package.swift
+++ Package.swift
@@ -13,15 +13,14 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/admost/AMR-IOS-SDK.git", from: "1.5.84"),
-        .package(url: "https://github.com/Unity-Technologies/unity-ads-ios.git", .exact("4.17.0"))
+        .package(url: "https://github.com/admost/AMR-IOS-SDK.git", from: "1.5.84")
     ],
     targets: [
         .target(
             name: "AMRAdapterUnity",
             dependencies: [
                 "AMRAdapterUnityLib",
-                .product(name: "UnityAds", package: "unity-ads-ios"),
+                "UnityAdsLib",
                 .product(name: "AMRSDK", package: "AMR-IOS-SDK")
             ],
             path: "AMRAdapterUnity",
@@ -34,6 +33,11 @@ let package = Package(
             name: "AMRAdapterUnityLib",
             url: "https://github.com/admost/AMR-IOS-ADAPTER-UNITYADS/releases/download/4.17.0/AMRAdapterUnity.xcframework.zip",
             checksum: "ac2aec4f7e9fa7d8aeaba5cde5a11062ee5dafc75bab467836f5a153c92b2dd1"
+        ),
+        .binaryTarget(
+            name: "UnityAdsLib",
+            url: "https://github.com/Unity-Technologies/unity-ads-ios/releases/download/4.17.0/UnityAds.zip",
+            checksum: "8a40e6c779353e4ead87d4d635480cdae46f0b708763460dc002103e35ce8ba7"
         )
     ]
 )
